43eac6271053fb9f986d5de0f3b06078a4f4b8e0
Utkarsh <utkarsh2001saxena@gmail.com>
2021-09-14 13:12:40 +0530

    khu

--- navigator/views.py
+++ navigator/views.py
@@ -1,3 +1,3 @@
 from django.http import HttpResponse
 def links(request):
-    return HttpResponse('<h1>Anime Lst</h1>\n <a href="https://gogoanime.pe/category/kanojo-okarishimasu">Rental Girlfriend</a><br><a href="https://gogoanime.vc/category/fairy-tail">Fairy Tail</a><br><a href="href="https://gogoanime.vc/category/gintama">Gintama</a>')
+    return HttpResponse('<h1>Anime List</h1>\n <a href="https://gogoanime.pe/category/kanojo-okarishimasu">Rental Girlfriend</a><br><a href="https://gogoanime.vc/category/fairy-tail">Fairy Tail</a><br><a href="href="https://gogoanime.vc/category/gintama">Gintama</a>')
